9157e7b4cb61f8a3d9d920c4350a4032fb1d7ff5
Franz Huayta <fhuayta@agetic.gob.bo>
2019-11-18 17:52:21 -0400

    contenido datos

--- src/reducirimagen/Tushar2.java
+++ src/reducirimagen/Tushar2.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package reducirimagen;
 
 import javax.swing.ImageIcon;
@@ -19,23 +14,26 @@ import java.awt.RenderingHints;
 public class Tushar2 {
 
     public void scaleImage() {
+//        String rutaArchivo = "/home/franz/Descargas/apple.jpg";
+//        String rutaArchivoSaliente = "/home/franz/Descargas/apple50_3.jpg";
+        String rutaArchivo = "/home/franz/cherry.jpg";
+        String rutaArchivoSaliente = "/home/franz/cherry-modificado.jpg";
         try {
 //            ImageIcon ii = new ImageIcon("/tmp/apple.jpg");
-            ImageIcon ii = new ImageIcon("/home/franz/Descargas/apple.jpg");
-            BufferedImage bi = new BufferedImage(100, 100, BufferedImage.TYPE_INT_RGB);
+            ImageIcon ii = new ImageIcon(rutaArchivo);
+            BufferedImage bi = new BufferedImage(300, 300, BufferedImage.TYPE_INT_RGB);
             Graphics2D g2d = (Graphics2D) bi.createGraphics();
             g2d.addRenderingHints(new RenderingHints(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY));
 //            boolean b = g2d.drawImage(ii.getImage(), 0, 0, 50, 50, null); // Baja la calidad
-            boolean b = g2d.drawImage(ii.getImage(), 0, 0, 100, 100, null); // Baja la calidad
+            boolean b = g2d.drawImage(ii.getImage(), 0, 0, 300, 300, null); // Baja la calidad
             System.out.println(b);
 //            ImageIO.write(bi, "jpg", new File("/tmp/apple50.jpg"));
-            ImageIO.write(bi, "jpg", new File("/home/franz/Descargas/apple50_3.jpg"));
+            ImageIO.write(bi, "jpg", new File(rutaArchivoSaliente));
         } catch (Exception e) {
             System.out.println("*** = " + e.getMessage());
             e.printStackTrace();
         }
     }
-
     public static void main(String[] args) {
         new Tushar2().scaleImage();
     }
